d8f337e7ec5a6d9f2ab50a07f3f2e1d6951eb19a
christopher <mboussachristopher@gmail.com>
2018-12-10 15:09:06 +0100

    suppression de l url : ^CompteUtilisateur$

--- CompteUtilisateur/urls.py
+++ CompteUtilisateur/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.connexion),
     url(r'^connexion$',views.connexion),
     url(r'^nvMDP$',views.nvMDP),
 ]
